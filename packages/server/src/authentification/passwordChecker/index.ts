fe1c98c967bd7ee1f85f6bf19f301ab3c9b0b66a
Grigory <techuila.sunset@gmail.com>
2025-04-29 12:39:14 +0500

    refactor(server/authentication/passwordChecker): decouple checking logic from elysia

--- packages/server/src/authentification/passwordChecker/index.ts
+++ packages/server/src/authentification/passwordChecker/index.ts
@@ -25,25 +25,34 @@ export class PasswordChecker implements Macro {
   }
 
   public get plugin() {
-    const password = this.#password;
     const logger = this.#logger;
-    const skipOnDevelopment = this.#skipOnDevelopment;
+    const checkPassword = this.#checkPassword.bind(this);
 
     return new Elysia({ name: PasswordChecker.pluginName }).macro({
       [PasswordChecker.macroName]: {
         beforeHandle({ error, headers }) {
-          if (process.env.NODE_ENV === "development" && skipOnDevelopment) {
-            logger?.warn("Skipping password check in development mode");
-            return;
-          }
-
-          logger?.info("Checking password...");
-          if (headers.password !== password) {
-            logger?.error("Password check failed. Sending error...");
+          if (!checkPassword(headers.password)) {
+            logger?.info("Sending error...");
             return error(401);
           }
         },
       },
     });
+  }
+
+  #checkPassword(password?: string): boolean {
+    if (process.env.NODE_ENV === "development" && this.#skipOnDevelopment) {
+      this.#logger?.warn("Skipping password check in development mode");
+      return true;
+    }
+
+    this.#logger?.info("Checking password...");
+    if (password !== this.#password) {
+      this.#logger?.error("Password check failed.");
+      return false;
+    }
+
+    this.#logger?.info("Password check passed.");
+    return true;
   }
 }
